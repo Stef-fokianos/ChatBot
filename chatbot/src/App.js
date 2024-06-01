fcd680d622d646e746e745c0d58d5763bc58af06
Stef_PC <stfokianos@gmail.com>
2024-06-01 19:39:43 +0300

    Minor changes

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './ΣF v1.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm trying to learn
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://sfdesigns.webflow.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn More about me
         </a>
       </header>
     </div>
